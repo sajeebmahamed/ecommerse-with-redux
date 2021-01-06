ff8cc522dfdac82d39a3a41eace7ff4298a207bc
Sajeeb <sajeebahamed6@gmail.com>
2021-01-06 23:53:37 +0600

    product screen ui done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main>
         <Container>
-          <HomeScreen />
+          <Route path='/' exact component={HomeScreen} />
+          <Route path='/product/:id' component={ProductScreen} /> 
         </Container>
       </main>
       <Footer /> 
-    </>
+    </Router>
   );
 }
 
