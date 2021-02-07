a4f357a4c89c133977411cc72863f6db8e8414ff
Sajeeb <sajeebahamed6@gmail.com>
2021-02-07 22:48:17 +0600

    fetch products from server

--- src/apiKey.js
+++ src/apiKey.js
@@ -1,1 +1,3 @@
-export const api = 'https://eco-proshop.herokuapp.com/api/'
+// export const api = 'http://cors-anywhere.herokuapp.com/https://eco-proshop.herokuapp.com/api/'
+export const api = 'http://localhost:5000/api'
+
--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { api } from '../apiKey';
 import Rating from '../components/Rating';
-import products from '../products';
 
 const ProductScreen = ({ match }) => {
-    const product = products.find((p) => p._id === match.params.id)
+    const [product, setProduct] = useState({})
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const {data} = await axios.get(`${api}/products/${match.params.id}`)
+            setProduct(data)
+        }
+        fetchProduct() 
+    }, [])
     return (
         <>
             <Link className='btn btn-light my-3' to='/'>
@@ -13,7 +21,7 @@ const ProductScreen = ({ match }) => {
             </Link>   
             <Row>
                 <Col md={6}>
-                    <Image src={product.image} alt={product.name} />
+                    <Image width='100%' src={product.image} alt={product.name} />
                 </Col>
                 <Col md={3}>
                     <ListGroup variant='flush'>
